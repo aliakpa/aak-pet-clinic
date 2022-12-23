99d2bee5b0d2a7edf2eb7f825d49916a15849607
Ali Ak <aliakbartin@gmail.com>
2022-12-22 22:48:11 -0500

    Initial commit

---
+++ src/main/java/com/aliak/petclinic/aakpetclinic/AakPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.aliak.petclinic.aakpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AakPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AakPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aliak/petclinic/aakpetclinic/AakPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliak.petclinic.aakpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AakPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
